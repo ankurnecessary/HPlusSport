dea1b2c0ca926fb95d9cf5f772bf2d6652be28bb
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-06-08 05:05:48 +0930

    Initial category model.

---
+++ HPlusSport.API/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; } = String.Empty;
+        public virtual List<Product> Products { get; set; }
+    }
+}
