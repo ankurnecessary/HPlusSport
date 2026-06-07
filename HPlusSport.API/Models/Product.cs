5495beaaa0f2dd3bc7934a1600db799f439872a4
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-06-08 04:42:02 +0930

    Initial product model.

---
+++ HPlusSport.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string Sku { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
